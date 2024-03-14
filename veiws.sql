d9d21591a5380828f1eb04e2fb24baf8adb25940
Kevin <walshkev@oregonstate.edu>
2024-03-14 11:27:42 -0700

    working on triggers

--- veiws.sql
+++ veiws.sql
@@ -5,17 +5,30 @@
 CREATE VIEW DiscountCounts AS
 SELECT
     current_date AS date,
-    COUNT(DISTINCT CASE WHEN ci.is_discounted = TRUE THEN ci.item_id END) AS total_discounted_items,
-    COUNT(DISTINCT CASE WHEN ci.is_discounted = TRUE AND ci.item_id NOT IN (
+    COUNT(DISTINCT CASE WHEN companyItem.is_discounted = TRUE THEN companyItem.item_id END) AS total_discounted_items,
+    COUNT(DISTINCT CASE WHEN companyItem.is_discounted = TRUE AND companyItem.item_id NOT IN (
         SELECT item_id FROM CompanyItem WHERE is_discounted = TRUE AND company_id IN (
             SELECT company_id FROM CompanyTransaction WHERE transaction_date = current_date - INTERVAL '1 day'
         )
-    ) THEN ci.item_id END) AS new_discounted_items
+    ) THEN companyItem.item_id END) AS new_discounted_items
 FROM
-    CompanyItem ci
-    INNER JOIN Discount d ON ci.discount_id = d.discount_id
+    CompanyItem companyItem
+    INNER JOIN Discount d ON companyItem.discount_id = d.discount_id
 GROUP BY
     current_date;
+
+
+-- CREATE VIEW DiscountCount AS
+-- SELECT
+--     current_date AS date,
+--     COUNT(*) AS total_discounted_items,
+--     COUNT(CASE WHEN discount_id IS NULL THEN 1 END) AS new_discounted_items
+-- FROM
+--     Discount
+-- WHERE
+--     discount_percentage > 0
+-- GROUP BY
+--     current_date;
 
 -- C2
 CREATE VIEW SortedProductList AS
