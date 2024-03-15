1b915844f369cd59383888afb1a2a045edecf7bf
Braxton Lowe <56900096+CobaltRebellion@users.noreply.github.com>
2024-03-15 16:12:30 -0700

    added employee_role_index

--- indices.sql
+++ indices.sql
@@ -1,2 +1,4 @@
 -- other indexes were created when creating the table with the unique constraint
 CREATE INDEX item_price_index ON item USING btree (item_price);
+
+CREATE INDEX employee_role_index ON employee USING hash (job_category);
